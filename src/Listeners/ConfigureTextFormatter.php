f8eff75575e53c32be529dd9b4c8292df60fc9df
Tassadar <lago@psych.ucsb.edu>
2021-01-30 17:30:18 -0500

    Check for tags

--- src/Listeners/ConfigureTextFormatter.php
+++ src/Listeners/ConfigureTextFormatter.php
@@ -38,36 +38,40 @@ class ConfigureTextFormatter
     public function textFormatterConfigurator(Configuring $event)
     {
       // Modify the IMG BBCode
-      $imgTag = $event->configurator->tags['IMG'];
-      $attribute = $imgTag->attributes->add('fancy');
-      $attribute->required = false;
-      $attribute->filterChain->append('#choice')->setValues(['off']);
+      if(array_key_exists("IMG",$event->configurator->tags)){
+        $imgTag = $event->configurator->tags['IMG'];
+        $attribute = $imgTag->attributes->add('fancy');
+        $attribute->required = false;
+        $attribute->filterChain->append('#choice')->setValues(['off']);
 
-      $imgTag->template = preg_replace(
-      	'(^<[^>]++>\\K)',
-      	'<xsl:if test="@fancy = \'off\'"><xsl:attribute name="data-nothing-fancy"/></xsl:if>',
-      	$imgTag->template
-      );
+        $imgTag->template = preg_replace(
+          '(^<[^>]++>\\K)',
+          '<xsl:if test="@fancy = \'off\'"><xsl:attribute name="data-nothing-fancy"/></xsl:if>',
+          $imgTag->template
+        );
+      }
 
       // Modify the URL BBCode
-      $urlTag = $event->configurator->tags['URL'];
-      $attribute = $urlTag->attributes->add('fancy');
-      $attribute->required = false;
-      $attribute->filterChain->append('#choice')->setValues(['video', 'iframe']);
+      if(array_key_exists("URL",$event->configurator->tags)){
+        $urlTag = $event->configurator->tags['URL'];
+        $attribute = $urlTag->attributes->add('fancy');
+        $attribute->required = false;
+        $attribute->filterChain->append('#choice')->setValues(['video', 'iframe']);
 
-      $urlTag->template = preg_replace(
-      	'(^<[^>]++>\\K)',
-      	'<xsl:choose>
-           <xsl:when test="@fancy = \'iframe\'">
-             <xsl:attribute name="class">fancybox--iframe-link</xsl:attribute>
-           </xsl:when>
-           <xsl:otherwise>
-            <xsl:if test="@fancy = \'video\'">
-              <xsl:attribute name="class">fancybox--video-link</xsl:attribute>
-            </xsl:if>
-           </xsl:otherwise>
-         </xsl:choose>',
-      	$urlTag->template
-      );
+        $urlTag->template = preg_replace(
+          '(^<[^>]++>\\K)',
+          '<xsl:choose>
+            <xsl:when test="@fancy = \'iframe\'">
+              <xsl:attribute name="class">fancybox--iframe-link</xsl:attribute>
+            </xsl:when>
+            <xsl:otherwise>
+              <xsl:if test="@fancy = \'video\'">
+                <xsl:attribute name="class">fancybox--video-link</xsl:attribute>
+              </xsl:if>
+            </xsl:otherwise>
+          </xsl:choose>',
+          $urlTag->template
+        );
+      }
     }
 }
